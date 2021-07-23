6e7aac5f44aa576a596942acba4879e2e805a0ff
Your Name <hermanliu95@gmailcom>
2021-07-23 12:31:29 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets learn Redux</h1>
+      <p>First we need to install the package for it</p>
+    </div>
+  );
+}
+
+export default App;
